dee58435520fc2b223e1f97d4b3c2ad89e8e8aa1
xaviercartytkww <xaviercarty@gmail.com>
2024-12-05 09:44:21 -0500

    get search orking

--- server/api/recipes.js
+++ server/api/recipes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.get('/search', authenticateToken, async (req, res) => {
     try {
         const { searchTerm } = req.query;
-        const appId = process.env.EDAMAM_APP_ID;
-        const appKey = process.env.EDAMAM_APP_KEY;
+        const appId = process.env.EDAMAM_RECIPE_APP_ID;
+        const appKey = process.env.EDAMAM_RECIPE_APP_KEY;
 
         const response = await axios.get(
             `https://api.edamam.com/api/recipes/v2?type=public&q=${searchTerm}&app_id=${appId}&app_key=${appKey}`
